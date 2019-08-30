da715c0f66bc7ee1c3149dcab4d074d17d003d40
jjun1 <jjun1@DESKTOP-3Q2MQVR>
2019-08-31 00:38:17 +0900

    main

---
+++ src/com/kh/model/vo/User.java
@@ -0,0 +1,53 @@
+package com.kh.model.vo;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class User {
+	private Image img = new ImageIcon("hello\\yangpa.PNG").getImage().getScaledInstance(150, 150, 0);
+	private boolean profile = true;
+	private int musicVolume = 30;
+	
+	public User() {}
+
+	
+	public User(Image img, boolean profile, int musicVolume) {
+		super();
+		this.img = img;
+		this.profile = profile;
+		this.musicVolume = musicVolume;
+	}
+
+
+	public Image getImg() {
+		return img;
+	}
+
+	public void setImg(Image img) {
+		this.img = img;
+	}
+
+	public boolean getProfile() {
+		return profile;
+	}
+
+	public void setProfile(boolean profile) {
+		this.profile = profile;
+	}
+
+	
+
+	public int getMusicVolume() {
+		return musicVolume;
+	}
+
+	public void setMusicVolume(int musicVolume) {
+		this.musicVolume = musicVolume;
+	}
+	
+	
+
+	
+	
+}
