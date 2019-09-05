5c5e91227a39139edea9738943dae8b26cb8a391
jjun1 <jjun1@DESKTOP-3Q2MQVR>
2019-09-06 01:30:12 +0900

    0906_10

---
+++ src/com/kh/model/vo/Quest.java
@@ -0,0 +1,16 @@
+package com.kh.model.vo;
+
+import com.kh.user.controller.UserManager;
+
+public class Quest {
+	UserManager um = new UserManager();
+
+	public void startQuset() {
+		String[] qusetList = new String[3];
+		qusetList[0] = "코인100개소유";
+		qusetList[1] = "주먹밥쿵야 10개 모으기";
+		qusetList[2] = "처음으로 우승해보기";
+
+	}
+
+}
