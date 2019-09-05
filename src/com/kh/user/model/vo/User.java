fb0ae8883d3fb962f5b8d422ce723063619eefa0
jjun1 <jjun1@DESKTOP-3Q2MQVR>
2019-09-06 00:35:28 +0900

    3

--- src/com/kh/user/model/vo/User.java
+++ src/com/kh/user/model/vo/User.java
@@ -20,30 +20,77 @@ public class User implements Serializable {
 	private int victory;
 	private int ownItem1;
 	private int ownItem2;
+	boolean acceptQuest1 = false;
+	boolean acceptQuest2 = false;
+	boolean acceptQuest3 = false;
+	boolean questClear1 = false;
+	boolean questClear2 = false;
+	boolean questClear3 = false;
 
 	public User() {
 		this.coin = 0;
 		this.profile = true;
 		this.musicSet = true;
 		this.victory = 0;
+		this.acceptQuest1 = false;
+		this.acceptQuest2 = false;
+		this.acceptQuest3 = false;
+		this.questClear1 = false;
+		this.questClear2 = false;
+		this.questClear3 = false;
 		this.ownItem1 = 0;
 		this.ownItem2 = 0;
 	}
 
-	public int getOwnItem1() {
-		return ownItem1;
+	public boolean getAcceptQuest1() {
+		return acceptQuest1;
 	}
 
+	public void setAcceptQuest1(boolean acceptQuest1) {
+		this.acceptQuest1 = acceptQuest1;
+	}
+	public boolean getAcceptQuest2() {
+		return acceptQuest2;
+	}
 	public int getOwnItem2() {
 		return ownItem2;
 	}
 
+	public void setAcceptQuest2(boolean acceptQuest2) {
+		this.acceptQuest2 = acceptQuest2;
+	}
+	public boolean getAcceptQuest3() {
+		return acceptQuest3;
+	}
+
+	public void setAcceptQuest3(boolean acceptQuest3) {
+		this.acceptQuest3 = acceptQuest3;
+	}
+	public boolean getQuestClear1() {
+		return questClear1;
+	}
 	public void setOwnItem1(int ownItem1) {
 		this.ownItem1 = ownItem1;
+	}
+	public void setQuestClear1(boolean questClear1) {
+		this.questClear1 = questClear1;
 	}
 
 	public void setOwnItem2(int ownItem2) {
 		this.ownItem2 = ownItem2;
+	}
+	public boolean getQuestClear2() {
+		return questClear2;
+	}
+	public void setQuestClear2(boolean questClear2) {
+		this.questClear2 = questClear2;
+	}
+	public boolean getQuestClear3() {
+		return questClear3;
+	}
+
+	public void setQuestClear3(boolean questClear3) {
+		this.questClear3 = questClear3;
 	}
 
 	public String getUserId() {
@@ -118,6 +165,12 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName + ", email=" + email
+				+ ", imgName = " + imgName + ", coin=" + coin + ", profile=" + profile + ", musicSet=" + musicSet + ", victory="
+				+ victory + "]";
+	}
 	public String getImgName() {
 		return imgName;
 	}
