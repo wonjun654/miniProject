5e4ec105791cbbc917b893c82c192eaf5e963cdd
user2 <user2@KH_C>
2019-09-06 19:02:10 +0900

    02

--- src/com/kh/view/QuestMenu.java
+++ src/com/kh/view/QuestMenu.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,12 +20,12 @@ public class QuestMenu extends JFrame {
 
 	ImageIcon background;
 
-
 	public QuestMenu(User user) {
-		String[] qusetList = new String[3];
-		qusetList[0] = "코인100개소유";
-		qusetList[1] = "주먹밥쿵야 10개 모으기";
-		qusetList[2] = "처음으로 우승해보기";
+		String[] questList = new String[4];
+		questList[0] = "코인100개소유"; // 시간아이템 + 10
+		questList[1] = "주먹밥쿵야 10개 모으기"; // 초성아이템 + 10
+		questList[2] = "처음으로 우승해보기"; // 사진.
+		
 
 		UserManager um = new UserManager();
 		this.setBounds(420, 100, 600, 600);
@@ -42,174 +43,214 @@ public class QuestMenu extends JFrame {
 		Panel.setSize(600, 600);
 		/////////////////////////////////////////////////////
 
-		JTextField questList1 = new JTextField(qusetList[0], 15);
+		JTextField questList1 = new JTextField(questList[0], 15);
 		questList1.setEditable(false);
 		questList1.setLocation(50, 70);
 		questList1.setSize(300, 60);
-		questList1.setBackground(Color.BLUE);
+		questList1.setBackground(Color.white);
 
-		JTextField questList2 = new JTextField(qusetList[1], 15);
+		JTextField questList2 = new JTextField(questList[1], 15);
 		questList2.setEditable(false);
 		questList2.setLocation(50, 190);
 		questList2.setSize(300, 60);
-		questList2.setBackground(Color.BLUE);
+		questList2.setBackground(Color.white);
 
-		JTextField questList3 = new JTextField(qusetList[2], 15);
+		JTextField questList3 = new JTextField(questList[2], 15);
 		questList3.setEditable(false);
 		questList3.setLocation(50, 310);
 		questList3.setSize(300, 60);
-		questList3.setBackground(Color.BLUE);
+		questList3.setBackground(Color.white);
 
 		JLabel title = new JLabel("퀘스트 목록");
 		title.setSize(150, 60);
 		title.setLocation(250, 0);
 
-		RoundButton button = new RoundButton("확인");
+		JButton button = new JButton("확인");
 		button.setSize(80, 30);
 		button.setLocation(470, 480);
 
-		RoundButton clearButton1 = new RoundButton("보상받기");
+		String change[] = new String[3];
+		change[0] = "수락";
+		change[1] = "진행중";
+		change[2] = "완료";
+
+		//////////////////////////////////////////////////////////////////////
+		JButton sdButton = new JButton("아이템 받기 !");
+		sdButton.setSize(120, 40);
+		sdButton.setLocation(150, 300);
+		
+		JLabel sdlabel = new JLabel(new ImageIcon("images/yangpa.PNG"));
+		sdlabel.setLocation(130, 60);
+		sdlabel.setSize(150, 150);
+		
+		Dialog sd = new Dialog(this, "^^");
+		sd.setSize(400, 400);
+		sd.setLocation(512, 250);
+		sd.setLayout(null);
+		sd.add(sdButton);
+		sd.add(sdlabel);
+		
+		JButton clearButton1 = new JButton("수락");
 		clearButton1.setSize(80, 30);
 		clearButton1.setLocation(470, 80);
 
-		RoundButton clearButton2 = new RoundButton("보상받기");
+		if(user.getAcceptQuest1()==true) {
+			clearButton1.setText("진행중");
+			clearButton1.setEnabled(false);
+		}
+		
+		if(user.getCoin()>=100) {
+			clearButton1.setText("완료");
+			clearButton1.setEnabled(true);
+		}
+
+		
+		JButton clearButton2 = new JButton("수락");
 		clearButton2.setSize(80, 30);
 		clearButton2.setLocation(470, 200);
+		
+		if(user.getAcceptQuest2()==true) {
+			clearButton2.setText("진행중");
+			clearButton2.setEnabled(false);
+			
+		}
+		
+		if(user.getOwnItem1()>=10) {
+			clearButton2.setText("완료");
+			clearButton2.setEnabled(true);
+		}
 
-		RoundButton clearButton3 = new RoundButton("보상받기");
+
+		JButton clearButton3 = new JButton("수락");
 		clearButton3.setSize(80, 30);
 		clearButton3.setLocation(470, 325);
-
-		RoundButton acceptButton1 = new RoundButton("수락");
-		acceptButton1.setSize(80, 30);
-		acceptButton1.setLocation(380, 80);
+		
 		if(user.getAcceptQuest1()==true) {
-			acceptButton1.setEnabled(false);
+			clearButton1.setText("진행중");
+			clearButton1.setEnabled(false);
 		}
-		RoundButton acceptButton2 = new RoundButton("수락");
-		acceptButton2.setSize(80, 30);
-		acceptButton2.setLocation(380, 200);
+		
+		if(user.getVictory()>=1) {
+			clearButton1.setText("완료");
+			clearButton1.setEnabled(true);
+		}
 
-		if(user.getAcceptQuest2()==true) {
-			acceptButton2.setEnabled(false);
-		}
-		RoundButton acceptButton3 = new RoundButton("수락");
-		acceptButton3.setSize(80, 30);
-		acceptButton3.setLocation(380, 325);
-		
-		if(user.getAcceptQuest3()==true) {
-			acceptButton3.setEnabled(false);
-		}
-		RoundButton sdButton = new RoundButton();
-		sdButton.setSize(60,60);
-		sdButton.setLocation(60, 60);
-	
-		
-		Dialog sd = new Dialog(this,"^^");
-		sd.setBounds(800, 250, 400, 500);
-		sd.setLayout(null);
-		sd.add(sdButton);
-		
-		sdButton.addActionListener(new ActionListener() {
+
+		clearButton1.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-					sd.setVisible(false);
-			}
-		});
-		
-		button.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-
-			}
-		});
-
-		clearButton1.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(user.getAcceptQuest1()==true&&um.selectOneUser("123").getCoin()>=100) {
-					System.out.println("줌");
+				user.setAcceptQuest1(true); 
+				if (user.getAcceptQuest1() ==true) {
+					clearButton1.setText("진행중");
+					clearButton1.setEnabled(false);
+				}
+				
+				if (user.getCoin()>=100) {
+					clearButton1.setText("퀘스트 완료");
+					user.setQuestClear1(true);
+					clearButton1.setEnabled(true);
 					sd.setVisible(true);
 					
 				}
+				
+				um.updateUser(user);
 			}
+			
 		});
 		
 		clearButton2.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(user.getAcceptQuest2()==true&&um.selectOneUser("123").getOwnItem1()>=10) {
-					System.out.println("줌");
+			
+			@Override public void actionPerformed(ActionEvent e) {
+				user.setAcceptQuest2(true); 
+				if (user.getAcceptQuest2() ==true) {
+					clearButton2.setText("진행중");
+					clearButton2.setEnabled(false);
+				}
+				
+				if (user.getOwnItem1()>=10) {
+					clearButton2.setText("퀘스트 완료");
+					user.setQuestClear2(true);
+					clearButton2.setEnabled(true);
 					sd.setVisible(true);
 					
 				}
-
-			}
-		});
-
-		clearButton3.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(user.getAcceptQuest3()==true&&um.selectOneUser("123").getVictory()>=1) {
-					System.out.println("3333줌");
-					sd.setVisible(true);
-				}
-			}
-		});
-
-		acceptButton1.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				acceptButton1.setEnabled(false);
-				user.setAcceptQuest1(true); 
-				um.updateUser(user);
 				
+				um.updateUser(user);
+			}
 			
-
-			}
 		});
 		
-		acceptButton2.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				acceptButton2.setEnabled(false);
-				user.setAcceptQuest2(true); 
-				um.updateUser(user);
-
-			}
-		});
-
-		acceptButton3.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				acceptButton3.setEnabled(false);
+		clearButton3.addActionListener(new ActionListener() {
+			
+			@Override public void actionPerformed(ActionEvent e) {
 				user.setAcceptQuest3(true); 
+				if (user.getAcceptQuest3() ==true) {
+					clearButton3.setText("진행중");
+					clearButton3.setEnabled(false);
+				}
+				
+				if (user.getVictory()>=1) {
+					clearButton3.setText("퀘스트 완료");
+					user.setQuestClear3(true);
+					clearButton3.setEnabled(true);
+					sd.setVisible(true);
+					
+				}
+				
 				um.updateUser(user);
-
 			}
+			
 		});
 		
+		sdButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				if (um.selectOneUser("123").getQuestClear1() == true) {
+					questList1.setText("퀘스트 완료");
+					clearButton1.setEnabled(false);
+				}
+				if (um.selectOneUser("123").getQuestClear2() == true) {
+					// 보상2
+					questList2.setText("퀘스트 완료");
+					clearButton2.setEnabled(false);
+				}
+				if (um.selectOneUser("123").getQuestClear3() == true) {
+					// 보상3
+					questList3.setText("퀘스트 완료");
+					clearButton3.setEnabled(false);
+				}
+
+				sd.setVisible(false);
+			}
+		});
+
+
 		
+
+		  
+		  button.addActionListener(new ActionListener() {
+			  
+			  @Override
+			  public void actionPerformed(ActionEvent e) {
+				  dispose();
+				  
+			  }
+		  });
 		
-		
+		  
+
 		this.add(Panel);
 
 		Panel.add(button);
 		Panel.add(clearButton1);
 		Panel.add(clearButton2);
 		Panel.add(clearButton3);
-		Panel.add(acceptButton1);
-		Panel.add(acceptButton2);
-		Panel.add(acceptButton3);
+		/*
+		 * Panel.add(acceptButton1); Panel.add(acceptButton2); Panel.add(acceptButton3);
+		 */
 
 		Panel.add(title);
 		Panel.add(questList1);
